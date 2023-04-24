d5d8a6132cfdaf8087245215ecd8a2051cc24b57
Ankush Swarnakar <ankushswar1@gmail.com>
2023-04-24 16:53:44 -0400

    Cleanup

--- components/onramp.tsx
+++ components/onramp.tsx
@@ -1,5 +1,6 @@
 import { Fragment } from "react";
 import { Dialog, Transition } from "@headlessui/react";
+import { InformationCircleIcon } from "@heroicons/react/24/solid";
 
 type OnRampModalProps = {
   onRampUrl: string | null;
@@ -34,28 +35,33 @@ export default function OnRampModal({ onRampUrl, onClose }: OnRampModalProps) {
               leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
             >
               <Dialog.Panel className="relative my-8 min-w-fit transform overflow-hidden rounded-lg bg-white px-4 pb-4 pt-2 text-left shadow-xl transition-all sm:p-6">
-                {/* <div>
-                  <div className="mt-3 text-center sm:mt-3">
+                <div>
+                  <div className="text-center">
                     <Dialog.Title
                       as="h3"
                       className="text-base font-semibold leading-6 text-gray-900"
-                    >
-                      Fund your wallet
-                    </Dialog.Title>
+                    ></Dialog.Title>
                   </div>
-                </div> */}
-                <div className="my-4">
+                </div>
+                <div className="my-2">
                   <iframe
                     src={onRampUrl as string}
-                    className="overflow:hidden h-[620px] w-[400px] rounded-md"
+                    className="overflow:hidden mb-1 h-[620px] w-[400px] rounded-md"
                   />
-                  {/* <button
-                    type="button"
-                    className="inline-flex w-full justify-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-                    onClick={onClose}
-                  >
-                    Close
-                  </button> */}
+                  <div className="flex w-full items-center text-center text-xs text-gray-500">
+                    <InformationCircleIcon className="mr-1 h-4 w-4" />
+                    <p>
+                      Privy partners with Sardine to help you purchase
+                      crypto.&nbsp;
+                    </p>
+                    <a
+                      className="underline hover:text-blue-600"
+                      href="https://www.sardine.ai/"
+                    >
+                      Learn more
+                    </a>
+                    <p>.</p>
+                  </div>
                 </div>
               </Dialog.Panel>
             </Transition.Child>
