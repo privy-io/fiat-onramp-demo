c75481e222d4aba023a12ea955af31c43026c5f6
Ankush Swarnakar <ankushswar1@gmail.com>
2023-05-09 20:10:59 -0400

    Simplify theming

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,17 +3,8 @@ import { usePrivy, VERSION } from "@privy-io/react-auth";
 import Head from "next/head";
 import { useEffect } from "react";
 import { useRouter } from "next/router";
-import type { PrivyTheme } from "../src/types";
 
-export default function LoginPage({
-  themes,
-  theme,
-  setTheme,
-}: {
-  themes: Array<PrivyTheme>;
-  theme: PrivyTheme;
-  setTheme: (theme: PrivyTheme) => void;
-}) {
+export default function LoginPage() {
   const router = useRouter();
   const { ready, authenticated, login } = usePrivy();
 
@@ -28,24 +19,6 @@ export default function LoginPage({
       <Head>
         <title>Login · Privy</title>
       </Head>
-
-      <div className="absolute top-1 left-1 right-0 flex items-center justify-between px-2 py-2 text-slate-500">
-        <span>
-          <b>SDK Version</b>: {VERSION}
-        </span>
-        <select
-          value={theme.name}
-          onChange={(e) =>
-            setTheme(themes.find((theme) => theme.name === e.target.value)!)
-          }
-        >
-          {themes.map((theme: any) => (
-            <option key={theme.name} value={theme.name}>
-              {theme.name}
-            </option>
-          ))}
-        </select>
-      </div>
       <main className="flex min-h-screen min-w-[100vw]">
         <div className="flex flex-1 items-center justify-center bg-privy-light-blue p-6">
           <div>
